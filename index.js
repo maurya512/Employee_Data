94421b175c603cd0b2edb27fcfea4c0f11723f92
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-25 22:42:57 -0400

    inquirer function that asks the user to make selections

--- index.js
+++ index.js
@@ -30,3 +30,56 @@ connection.connect(function(err){
     employeeSearch();
 });
 
+// the function that's gonna ask the user to make a selection
+function employeeSearch() {
+    inquirer
+        .prompt({
+            type: "rawlist",
+            name: "employees",
+            message: "What would you like to do?",
+            choices: [
+                "Look up all the employees in the company.",
+                "Look up an employee's department.",
+                "Look up an employee's role.",
+                "Look up an employee's pay",
+                "Add a new employee.",
+                "EXIT"
+            ]
+        })
+        .then(function(choice){
+            switch(choice.employees){
+                case "Look up all the employees in the company.":
+                    // the function that displays all of the employees within a company 
+                    employeeData();
+                    break;
+                
+                case "Look up an employee's department.":
+                    // the function that displays the department of employee within the company
+                    empDepartment();
+                    break;
+                
+                case "Look up an employee's role.":
+                    // the function that displays the employee's role in the company
+                    // something to do with the id
+                    empRole();
+                    break;
+
+                case "Look up an employee's pay":
+                    // the function that displays the employee's pay 
+                    empPay();
+                    break;
+
+                case "Add a new employee.":
+                    // the function that adds a new employee, department, role and pay
+                    addNewEmployee();
+                    break;
+
+                case "EXIT":
+                    // if the user chooses "exit" end the connection 
+                    connection.end();
+
+            }
+        });
+}
+
+// 
