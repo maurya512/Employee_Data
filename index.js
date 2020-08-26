f8cc49d102edc94d071ce03ac80182b345b6ae28
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-25 22:26:11 -0400

    initial connection with the database made

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 // switch case with inquirer
 // inquirer prompt. if they choose this option run this case and so on and so forth
+
+// modules 
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create a connection with the database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // port
+    port: 8080,
+
+    // username
+    user: "root",
+
+    // password
+    password: process.env.MYSQL_KEY,
+    database: "employees"
+});
+
+// check if the connection has been made and if made run the function
+connection.connect(function(err){
+    if(err){
+        // if no connection has been made let us know
+        console.log(err);
+    }
+    // else run the function
+    employeeSearch();
+});
+
